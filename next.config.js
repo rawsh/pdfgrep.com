305c587cded639ed50023220c893187e14ea1cd4
Robert Washbourne <rawashbourne@gmail.com>
2022-11-17 18:27:42 -0500

    test preact change

--- next.config.js
+++ next.config.js
@@ -20,13 +20,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
+
+    // Note, preact is only enabled for production builds (`next build`)
     if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      };
     }
 
     return config;
