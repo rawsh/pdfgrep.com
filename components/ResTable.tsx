75a31ee500a135d0e885aefe913c1ead24cc2e3c
Robert Washbourne <rawashbourne@gmail.com>
2023-01-31 14:15:42 -0400

    fix view with fixed table-layout

--- components/ResTable.tsx
+++ components/ResTable.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import * as React from 'react'
-import { useState } from 'react'
-
 import styles from '../styles/ResTable.module.css'
 
 type ResTableData = {
@@ -21,20 +19,22 @@ export default function ResTable(props: ResTableProps) {
     const data = props.data
 
     return (
-        <div className={`${styles.results}`}>
-            {data.map((row, i) => {
-                let className = styles.result
-                if (props.currentRow === i) {
-                    className += ` ${styles.selected}`
-                }
-                return (
-                    <div className={className} key={i} onClick={() => props.onRowClick(row.filename, row.page, i)}>
-                        <div className={`${styles.filename} `}>{row.filename}</div>
-                        <div className={`${styles.page} `}>{row.page}</div>
-                        <div className={`${styles.text} `}>{row.text}</div>
-                    </div>                    
-                )
-            })}
+        <div className={styles.tableWrapper}>
+            <div className={`${styles.results}`}>
+                {data.map((row, i) => {
+                    let className = styles.result
+                    if (props.currentRow === i) {
+                        className += ` ${styles.selected}`
+                    }
+                    return (
+                        <div className={className} key={i} onClick={() => props.onRowClick(row.filename, row.page, i)}>
+                            <div className={`${styles.filename} `}>{row.filename}</div>
+                            <div className={`${styles.page} `}>{row.page}</div>
+                            <div className={`${styles.text} `}>{row.text}</div>
+                        </div>                    
+                    )
+                })}
+            </div>
         </div>
     )
 }
